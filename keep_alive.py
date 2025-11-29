25e925304a4a3aa6ecc7ab91a1b468ecd0153363
saoif2051496490-svg <saoif2051496490@gmail.com>
2025-11-29 16:34:52 +0900

    Update keep_alive.py

--- keep_alive.py
+++ keep_alive.py
@@ -8,7 +8,7 @@ def home():
     return "Bot is running!"
 
 def run():
-    app.run(host='0.0.0.0', port=8080)
+    app.run(host='0.0.0.0', port=10000)
 
 def keep_alive():
     t = Thread(target=run)
